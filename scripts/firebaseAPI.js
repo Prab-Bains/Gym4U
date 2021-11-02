aa708d07773ac43f0edc8ee45c20779630951bd8
GyoungHwan-Hong <hongkh5218@gmail.com>
2021-11-02 13:47:23 -0700

    Add logo images and implement login.html

---
+++ scripts/firebaseAPI.js
@@ -0,0 +1,11 @@
+var  firebaseConfig = {
+    apiKey: "AIzaSyDN8vIl9mp5gk_73rwP2-6h8MqiMjq41eg",
+    authDomain: "demo08-comp1800-a95ea.firebaseapp.com",
+    projectId: "demo08-comp1800-a95ea",
+    storageBucket: "demo08-comp1800-a95ea.appspot.com",
+    messagingSenderId: "902331516254",
+    appId: "1:902331516254:web:33214adf6336c360b4fbcf"
+    };
+    
+  
+    const app = firebase.initializeApp(firebaseConfig);
