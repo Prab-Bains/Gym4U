ff3783d0a3f75d9c04dc58eaf36036b8a2fbcc71
GyoungHwan-Hong <hongkh5218@gmail.com>
2021-12-03 11:30:37 -0800

    Add comments on appointments.js

--- scripts/appointments.js
+++ scripts/appointments.js
@@ -2,9 +2,10 @@
             firebase.auth().onAuthStateChanged(user => {
                 if (user) {
                     var uid = user.uid;
+                    // check user signen in.
                     var currentDB = db.collection("appointment_dictionary").doc(uid)
                     currentDB.get().then(doc => {
-                        var target_object_key = id.slice(-2);
+                        var target_object_key = id.slice(-2);   //id.slice(-2) means 'a1, a2 ... this is appointment identifier in screen'
                         var appointment_list = doc.data();
                         let new_appointment_list = appointment_list;
 
@@ -13,20 +14,20 @@
                         var from_time_id = "from_time_id_" + id.slice(-2)
                         var gym_name_id = "gym_name_id_" + id.slice(-2)
 
-                        var date_value = $("#" + date_id).val();
+                        var date_value = $("#" + date_id).val();                //get data to update from the page
                         var to_time_value = $("#" + to_time_id).val();
                         var from_time_value = $("#" + from_time_id).val();
                         var gym_name_id_value = $("#" + gym_name_id).text();
 
-                        let appointment_info_object = new Object({
+                        let appointment_info_object = new Object({              //make object to update from the page
                             date: date_value,
                             start_time: from_time_value,
                             end_time: to_time_value,
                             gym_name: gym_name_id_value
                         });
 
-                        new_appointment_list[target_object_key] = appointment_info_object;
-                        currentDB.set(new_appointment_list);
+                        new_appointment_list[target_object_key] = appointment_info_object; //update object
+                        currentDB.set(new_appointment_list);    //set appointment list object
                     })
                 }
             })
@@ -37,17 +38,17 @@
                 if (user) {
                     var uid = user.uid
                     var currentDB = db.collection("appointment_dictionary").doc(uid)
-                    var target_object_key = id.slice(-2);
+                    var target_object_key = id.slice(-2);   //id.slice(-2) means 'a1, a2 ... this is appointment identifier in screen'
                     currentDB.get().then(doc => {
                         var appointment_list = doc.data();
-                        delete appointment_list[target_object_key];
-                        let new_appointment_list = new Object({})
+                        delete appointment_list[target_object_key]; // delete current object from the appointment_list 
+                        let new_appointment_list = new Object({})   // make new appointment object since there are no way to delete only one element of appointment objects
                         let lengthOfAppointmentList = Object.keys(appointment_list).length;
                         var i = 1
                         for (val in appointment_list) {
                             new_appointment_list['a' + i] = appointment_list[val]
                             i++;
-                        }
+                        }                                           // re organize appointment object list.
                         console.log(new_appointment_list)
                         currentDB.set(new_appointment_list).then (
                             readAppointmentList()
@@ -59,29 +60,30 @@
         }
 
         function addAppointment() {
-            firebase.auth().onAuthStateChanged(user => {
+            firebase.auth().onAuthStateChanged(user => { // check user signen in
                 if (user) {
-                    var uid = user.uid
-                    var currentDB = db.collection("appointment_dictionary").doc(uid)
+                    var uid = user.uid // get user.uid
+                    var currentDB = db.collection("appointment_dictionary").doc(uid) //get collection (appointment lists)
                     currentDB.get().then(doc => {
                         if (doc.exists) {
                             var appointment_list = doc.data();
-                            let lengthOfAppointmentList = Object.keys(appointment_list).length + 1;
-                            let gymname = document.getElementById("id_gymname").value;
-                            let from_time = document.getElementById("id_from_time").value;
-                            let to_time = document.getElementById("id_to_time").value;
-                            let date = document.getElementById("id_date").value;
-                            let pointer = 'a' + lengthOfAppointmentList
+
+                            let lengthOfAppointmentList = Object.keys(appointment_list).length + 1; 
+                            let gymname = document.getElementById("id_gymname").value;              //assign appointment data from input tag.
+                            let from_time = document.getElementById("id_from_time").value;          //assign appointment data from input tag.
+                            let to_time = document.getElementById("id_to_time").value;              //assign appointment data from input tag.
+                            let date = document.getElementById("id_date").value;                    //assign appointment data from input tag.
+                            let pointer = 'a' + lengthOfAppointmentList                             //a + diciaml is unique id of an appointment. 
                             let new_appointment_list = appointment_list;
-                            let new_appointment_info = new Object({
+                            let new_appointment_info = new Object({                                 //get origin appointment list from firebase
                                 date: date,
                                 start_time: from_time,
                                 end_time: to_time,
                                 gym_name: gymname
                             });
-                            new_appointment_list[pointer] = new_appointment_info;
-                            currentDB.set(new_appointment_list);
-                        } else {
+                            new_appointment_list[pointer] = new_appointment_info;                   //Add new appointment object to origin appointment_list
+                            currentDB.set(new_appointment_list);                                    //Set new appointment list
+                        } else {                                                                    //In case that there are no appointment or user is new user.
                             let gymname = document.getElementById("id_gymname").value;
                             let from_time = document.getElementById("id_from_time").value;
                             let to_time = document.getElementById("id_to_time").value;
@@ -104,7 +106,7 @@
             })
         }
 
-        function readAppointmentList() {
+        function readAppointmentList() { // Print out appointments user have on the screen
             $(".appointment_container").empty();
             firebase.auth().onAuthStateChanged(user => {
                 if (user) {
@@ -114,15 +116,15 @@
                         if (doc.exists) {
                             var appointment_list = doc.data();
                             const key_list = Object.keys(appointment_list);
-                            let lengthOfAppointmentList = Object.keys(appointment_list).length;
-
-                            for (var i = lengthOfAppointmentList; i > 0; i--) {
+                            let lengthOfAppointmentList = Object.keys(appointment_list).length; // Get how many appointment the user  have from the database.
+                            
+                            for (var i = lengthOfAppointmentList; i > 0; i--) { // Append in reverse order to show the lastest apppointment first.
                                 pointer = 'a' + i;
                                 date = appointment_list[pointer]['date'];
                                 end_time = appointment_list[pointer]["end_time"];
                                 start_time = appointment_list[pointer]["start_time"];
                                 gym_name = appointment_list[pointer]["gym_name"];
-                                var containerform =
+                                var containerform = // ${pointer} will be used as identifier for appointment objects in firebase
                                     `<div class="card text-white bg-secondary mb-5" id="${pointer}" style="max-width: 18rem;">
                                         <div class="card-header" id="gym_name_id_${pointer}">${gym_name}</div>
                                             <div class="card-body">
@@ -147,10 +149,10 @@
         }
         readAppointmentList();
 
-        const open = () => {
+        const open = () => {    // Shows modal to add.
             document.querySelector(".modal").classList.remove("hidden");
         }
-        const close = () => {
+        const close = () => {   // Hides modal to add.
             document.querySelector(".modal").classList.add("hidden");
             readAppointmentList();
         }
